14227771916768390066656e36de855810d321c7
Daniel Milan <danielmilanvera@gmail.com>
2022-02-14 13:42:38 +0100

    Sign Up done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,28 +7,35 @@ const bcrypt = require("bcrypt");
 router
   .route("/signup")
 
-  .get((req, res) => {
+  .get((req, res, next) => {
     res.render("signup");
   })
 
-  .post((req, res) => {
+  .post((req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
 
     // Check the form is NOT EMPTY
-    if (!username || !password)
+    if (!username || !password) {
       res.render("sigup", { errorMessage: "All fields are required" });
+      return
+    } else {
+      User.findOne({ username }).then((user) => {
+        if (user && user.username) {
+          res.render("signup", { errorMessage: "User already exists" });
+        }
 
-    User.find({ username }).then((user) => {
-      if (user) {
-        res.render("signup", { errorMessage: "User already exists" });
-      }
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPwd = bcrypt.hashSync(password, salt);
 
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hashedPwd = bcrypt.hashSync(password, salt);
+        User.create({ username, password: hashedPwd })
+          .then((user) => res.render("profile", user))
 
-      User.create({ username, password: hashedPwd });
-    });
+          .catch((err) =>
+            res.render("signup", { errorMessage: `Error from DB: ${err} ` })
+          );
+      });
+    }
   });
 
 router.get("/login", (req, res) => {
