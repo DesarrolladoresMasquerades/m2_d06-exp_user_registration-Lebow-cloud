4cef0af0414705b5ba5a0fa8cf8d443811dc73ab
Daniel Milan <danielmilanvera@gmail.com>
2022-02-14 12:01:53 +0100

    Hashing PWD

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,19 +1,42 @@
-const express = require('express');
-const User = require('../models/User.model');
+const express = require("express");
+const User = require("../models/User.model");
 const router = express.Router();
-const saltRound = 10;
-const bcrypt = require('bcrypt');
+const saltRounds = 5;
+const bcrypt = require("bcrypt");
 
-router.get('/signup', (req, res, next) => {
-	res.render('signup');
+router
+  .route("/signup")
+
+  .get((req, res) => {
+    res.render("signup");
+  })
+
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check the form is NOT EMPTY
+    if (!username || !password)
+      res.render("sigup", { errorMessage: "All fields are required" });
+
+    User.find({ username }).then((user) => {
+      if (user) {
+        res.render("signup", { errorMessage: "User already exists" });
+      }
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPwd = bcrypt.hashSync(password, salt);
+
+      User.create({ username, password: hashedPwd });
+    });
+  });
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.get('/login', (req, res, next) => {
-	res.render('login');
-});
-
-router.get('/profile', (req, res) => {
-	res.render('profile');
+router.get("/profile", (req, res) => {
+  res.render("profile");
 });
 
 module.exports = router;
